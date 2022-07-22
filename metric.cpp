0bdae59be331bac4fd2a42f8ee53697461b73dd0
Dolev Dabush <dol12dab@gmail.com>
2022-07-22 16:59:37 +0300

    added metrics

---
+++ metric.cpp
@@ -0,0 +1,37 @@
+#include <stdexcept>
+#include <cmath>
+#include "metric.h"
+
+double EuclideanMetric::operator()(const std::vector<double> &t, const std::vector<double> &t1) {
+    if (t.size() != t1.size()) {
+        // TODO: change to more appropriate error
+        throw std::runtime_error("operands' sizes don't match");
+    }
+    double diff = 0;
+    for (int i = 0; i < t.size(); i++) {
+        diff += t.at(i) - t1.at(i);
+    }
+    return std::pow(diff, 1. / (double)t.size());
+}
+
+double ManhattanDistance::operator()(const std::vector<double> &t, const std::vector<double> &t1) {
+    if (t.size() != t1.size()) {
+        throw std::runtime_error("operands' sizes don't match");
+    }
+    double distance = 0;
+    for (int i = 0; i < t.size(); i++) {
+        distance += std::abs(t.at(i) - t1.at(i));
+    }
+    return distance;
+}
+
+double ChebyshevMetric::operator()(const std::vector<double> &t, const std::vector<double> &t1) {
+    if (t.size() != t1.size()) {
+        throw std::runtime_error("operands' sizes don't match");
+    }
+    double distance = 0;
+    for (int i = 0; i < t.size(); i++) {
+        distance = std::max(distance, std::abs(t.at(i) - t1.at(i)));
+    }
+    return distance;
+}
---
+++ metric.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+
+/// Measures distance between two data points.
+template <class T>
+class Metric {
+public:
+    virtual double operator()(const T&, const T&) = 0;
+};
+
+/// The usual Euclidean metric.
+class EuclideanMetric : public Metric<std::vector<double>> {
+public:
+    double operator()(const std::vector<double> &t, const std::vector<double> &t1) override;
+};
+
+/// The Manhattan distance function.
+class ManhattanDistance : public Metric<std::vector<double>> {
+public:
+    double operator()(const std::vector<double> &t, const std::vector<double> &t1) override;
+};
+
+/// The Chebyshev distance function.
+class ChebyshevMetric : public Metric<std::vector<double>> {
+public:
+    double operator()(const std::vector<double> &t, const std::vector<double> &t1) override;
+};
