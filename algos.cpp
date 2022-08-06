ad29391d49f0a695538126006fab5edb6d6b0f35
Dolev Dabush <dol12dab@gmail.com>
2022-08-06 15:41:57 +0300

    modified max_element to not require value_type to implement the assignment operator

--- algos.cpp
+++ algos.cpp
@@ -5,9 +5,9 @@ template <typename Iter, typename Pred>
 typename std::iterator_traits<Iter>::value_type
 utils::max_element(Iter begin, Iter end, Pred pred) {
     if (begin == end) throw std::logic_error("Invalid parameter: end and begin are equal");
-    auto max = *begin;
+    auto max = begin;
     while (++begin != end) {
-        if (pred(max, *begin)) max = *begin;
+        if (pred(*max, *begin)) max = begin;
     }
-    return max;
+    return *max;
 }
