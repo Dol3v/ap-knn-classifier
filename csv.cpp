7e2cfd46615f86ac245452d6148f24e63bfdcab4
Dolev Dabush <dol12dab@gmail.com>
2022-08-06 11:21:35 +0300

    split csv functionality into cpp, header and tpp files

--- csv.cpp
+++ csv.cpp
@@ -1,8 +1,6 @@
 #include "csv.h"
-#include <sstream>
 
 namespace csv {
-
     template <>
     std::string read_value(std::stringstream &sstream) {
         std::string return_value;
--- csv.h
+++ csv.h
@@ -27,38 +27,4 @@ namespace csv {
     void write_line(std::ostream &, const Values &...);
 }
 
-template<typename T>
-T csv::read_value(std::stringstream &sstream) {
-    char junk;
-    T return_value;
-    sstream >> return_value;
-    sstream >> junk; // remove comma delimiter
-    return return_value;
-}
-
-template<typename T, typename... Values>
-typename std::enable_if<sizeof...(Values) != 0, std::tuple<T, Values...>>::type
-csv::read_line(std::stringstream &sstream) {
-    T value = csv::read_value<T>(sstream);
-    std::tuple<T> value_tuple = std::make_tuple(value);
-    return std::tuple_cat(value_tuple, read_line<Values...>(sstream));
-}
-
-template<typename T>
-std::tuple<T> csv::read_line(std::stringstream &sstream) {
-    T value = csv::read_value<T>(sstream);
-    std::cout << value << std::endl;
-    return std::make_tuple(value);
-}
-
-template<typename T, typename... Values>
-void csv::write_line(std::ostream &os, const T &t, const Values &... values) {
-    // base case
-    if (sizeof...(values) == 0) {
-        os << t << std::endl;
-        return;
-    }
-    // "recursive" case
-    os << t << ",";
-    csv::write_line(os, values...);
-}
+#include "csv.tpp"
