dfe146a9c6910f2a5d4a0b9b3e3db68e6a85fefb
Dolev Dabush <dol12dab@gmail.com>
2022-08-06 15:40:22 +0300

    created test run

--- main.cpp
+++ main.cpp
@@ -1,17 +1,62 @@
+#include <fstream>
 #include "csv.h"
-
+#include "knn.h"
 
 int main() {
-//    std::string line = "123,29.5,19";
-    std::string s = "";
-    std::ostringstream os(s);
-    std::string str = "Iris";
-    csv::write_line<int, std::string, double>(os, 5, str, 8.5);
-//    std::istringstream iss(line);
-//    csv::read_line<int, double, int>(iss);
-//    std::cout << os.str() << std::endl;
+    ChebyshevMetric metric;
+    KnnClassifier<std::vector<double>, std::string> classifier(9, metric);
 
-    std::stringstream is(os.str());
-    auto tup = csv::read_line<int, std::string, double>(is);
-    std::cout << std::get<0>(tup) << " " << std::get<1>(tup) << " " << std::get<2>(tup) << std::endl;
+    std::fstream fio;
+
+    fio.open(R"(C:\Users\dol12\CLionProjects\ap1-knn-classifier\classified.csv)");
+
+    while (fio) {
+        std::string line;
+        std::getline(fio, line);
+
+        if (line.empty()) {
+            break;
+        }
+        std::stringstream ss(line);
+
+        auto values = csv::read_line<double, double, double, double, std::string>(ss);
+//        std::cout << "---" << std::endl;
+
+        auto data_class = std::get<4>(values);
+        std::vector<double> data;
+        data.push_back(std::get<0>(values));
+        data.push_back(std::get<1>(values));
+        data.push_back(std::get<2>(values));
+        data.push_back(std::get<3>(values));
+
+        classifier.load_data(data, data_class);
+    }
+
+    fio.close();
+    fio.open(R"(C:\Users\dol12\CLionProjects\ap1-knn-classifier\Unclassified.csv)");
+//    std::fstream fout;
+//    fout.open(R"(C:\Users\dol12\CLionProjects\ap1-knn-classifier\result.csv)", std::fstream::out | std::fstream::trunc);
+    if (!fio) {
+        std::cout << "Failed to open the necessary files" << std::endl;
+        return -1;
+    }
+
+    while (fio) {
+        std::string line;
+        std::getline(fio, line);
+        if (line.empty()) {
+            break;
+        }
+        std::stringstream ss(line);
+        auto values = csv::read_line<double, double, double, double>(ss);
+        std::vector<double> data;
+        data.push_back(std::get<0>(values));
+        data.push_back(std::get<1>(values));
+        data.push_back(std::get<2>(values));
+        data.push_back(std::get<3>(values));
+
+        std::cout << classifier.predict(data) << std::endl;
+    }
+
+    fio.close();
 }
